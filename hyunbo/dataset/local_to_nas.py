19449d4701960b1645d334088f1a4d3bdfa59051
hyunbo9 <7hyll7@gmail.com>
2023-01-16 06:37:06 +0000

    update hyunbo module

--- hyunbo/dataset/local_to_nas.py
+++ hyunbo/dataset/local_to_nas.py
@@ -10,19 +10,18 @@ def main():
     ]
 
     for folder in folder_list:
-        nas_to_local(folder)
+        move_to_nas(folder)
 
-def nas_to_local(folder):
-    
+def move_to_nas(folder):
+
     # if folder is already exist.
-    if os.path.exists(folder):
+    if os.path.exists(os.path.join(NAS_ADDRESS, folder)):
         if input(f"{folder} is already exist. do you want remove?\n") == 'y':
-            shutil.rmtree(folder)
-        print(f"finish removing {folder}")
+            shutil.rmtree(os.path.join(NAS_ADDRESS, folder))
 
-    print(f"##### copy progress in {folder} #####")
-    shutil.copytree(os.path.join(NAS_ADDRESS, folder), folder)
-    print("##### copy progress finish #####")
+    print(f"##### progress in {folder} #####")
+    shutil.copytree(folder, os.path.join(NAS_ADDRESS, folder))
+
 
 if __name__ == "__main__":
     main()
